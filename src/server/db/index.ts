2ae9f8b6834c61a8ccb6c7e2dfa50299487fa07f
Tye Peck <tye@novatasolutions.com.au>
2024-03-08 11:31:12 +0800

    fix: add missing types

--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -3,6 +3,11 @@ import postgres from "postgres";
 
 import { env } from "~/env";
 import * as schema from "./schema";
+import type {
+  BuildQueryResult,
+  DBQueryConfig,
+  ExtractTablesWithRelations,
+} from "drizzle-orm";
 
 /**
  * Cache the database connection in development. This avoids creating a new connection on every HMR
@@ -16,3 +21,24 @@ const conn = globalForDb.conn ?? postgres(env.POSTGRES_URL);
 if (env.NODE_ENV !== "production") globalForDb.conn = conn;
 
 export const db = drizzle(conn, { schema });
+
+type Schema = typeof schema;
+type TSchema = ExtractTablesWithRelations<Schema>;
+
+export type IncludeRelation<TableName extends keyof TSchema> = DBQueryConfig<
+  "one" | "many",
+  boolean,
+  TSchema,
+  TSchema[TableName]
+>["with"];
+
+export type InferResultType<
+  TableName extends keyof TSchema,
+  With extends IncludeRelation<TableName> | undefined = undefined,
+> = BuildQueryResult<
+  TSchema,
+  TSchema[TableName],
+  {
+    with: With;
+  }
+>;
